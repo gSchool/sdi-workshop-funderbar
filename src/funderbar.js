a4716217ec62117edd55bdc232e701760ef2bbaa
aaron <aaron6235@gmail.com>
2024-06-07 10:40:01 -0600

    second commit. Finished Part 1 variables.
    Co-authored-by: troutsniffr <troutsniffr@users.noreply.github.com>

--- src/funderbar.js
+++ src/funderbar.js
@@ -2,6 +2,7 @@
   'use strict';
 
   window._ = {};
+//hi aaron
 
   var FILL_ME_IN = 'Fill this value in';
 
@@ -10,21 +11,24 @@
 // to help guide you toward success in your work on the following exercises.
   _.define = function(input){
     // Declare a variable below that is assigned the value of input;
-    var result = FILL_ME_IN;
+    var result = input;
     // return the above variable
-    return FILL_ME_IN;
+    return input;
   }
 
 //_.dataType is wanting you to determine the data type of what is being passed into it.
 // Return the datatype of input: ie. string or number or boolean etc. 
 _.dataType = function(input){
   //Code Goes Below
-
+  let dataType = typeof input;
+  return dataType;
+  console.log(typeof input)
 }
 
 //_.sum takes two numbers as parameters and returns their sum
 _.sum = function(a,b){
     //Code Goes Below
+    return a + b;
     
 }
 
@@ -33,12 +37,14 @@ _.subtract = function(a,b){
   //Code Goes Below
   //For your code remember to subtract b from a
   //a - b
+  return a - b;
   
 }
 
 //_.multiply takes two numbers as parameters and returns their product
 _.multiply = function(a,b){
   //Code Goes Below
+  return a * b;
   
 }
 
@@ -46,6 +52,7 @@ _.multiply = function(a,b){
 _.divide = function(a,b){
   //Code Goes Below
   //a divided by b
+  return a / b;
   
 }
 
@@ -53,12 +60,15 @@ _.divide = function(a,b){
 _.remainderOperator = function(a,b){
   //Code Goes Below
   //return the remainder of a divided by b
+  return a % b;
 }
 
 //_.increment takes a number as a parameter and increces it by 1
 // Ensure you use the proper increment operator(not a+1)
 _.increment = function(a){
-  //Code Goes Below
+//If it is placed before the operand, it returns the value after the increment.
+//If it is placed after the operand, it returns the value before the increment.
+return ++a;
   
 }
 
@@ -66,6 +76,7 @@ _.increment = function(a){
 // Ensure you use the proper decrement operator (not a-1)
 _.decrement = function(a){
   //Code Goes Below
+  return --a;
   
 }
 
